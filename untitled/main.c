7fe7e14f8a62942513c5a3c2d56b3b8ee77f703e
maromo71 <professormoraes@gmail.com>
2025-05-24 12:18:54 -0300

    Aula do dia 24 de maio

---
+++ untitled/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+char * extrairSobrenome(char * p_nome);
+int main(void)
+{
+    printf("%s\n", extrairSobrenome("Marcos Roberto de Moraes"));
+    return 0;
+}
+char * extrairSobrenome(char * p_nome)
+{
+    char * p_sobrenome = NULL;
+
+    return p_sobrenome;
+}

d7fc15dc85fef617cb2093bf83f371c72c8ac78d
maromo71 <professormoraes@gmail.com>
2025-11-10 22:22:03 -0300

    Aula do dia 10 de novembro

--- aula1011/exemplo04/main.c
+++ aula1011/exemplo04/main.c
@@ -15,20 +15,20 @@ int main(void) {
         return -1;
     }
     char linha[256];
-    printf("=======================================NOTAS DOS ALUNOS================================\n");
+    printf("=======================================     NOTAS DOS ALUNOS    ================================\n");
     while (fgets(linha, sizeof(linha), p_notas)!=NULL) {
         if (sscanf(linha, "%49[^;];%f;%f", aluno.nome, &aluno.nota1, &aluno.nota2)==3) {
             aluno.media = (aluno.nota1 + aluno.nota2)/2;
             if (aluno.media >= 6) {
-                printf("Nome: %-20s | Nota 1: %-2.2f | Nota 2: %-2.2f | Media: %-2.2f | Situacao: Aprovado\n",
+                printf("Nome: %-20s | Nota 1: %5.2f | Nota 2: %5.2f | Media: %5.2f | Situacao: Aprovado\n",
                     aluno.nome, aluno.nota1, aluno.nota2, aluno.media);
             }else {
-                printf("Nome: %-20s | Nota 1: %-2.2f | Nota 2: %-2.2f | Media: %-2.2f | Situacao: Reprovado\n",
+                printf("Nome: %-20s | Nota 1: %5.2f | Nota 2: %5.2f | Media: %5.2f | Situacao: Reprovado\n",
                     aluno.nome, aluno.nota1, aluno.nota2, aluno.media);
             }
         }
     }
-    printf("===================================================================================\n");
+    printf("================================================================================================\n");
     fclose(p_notas);
 
 
